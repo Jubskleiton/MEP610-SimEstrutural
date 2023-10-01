02789891d3479d6c761e54b953f07b2910defaf0
Jubskleiton <enzo.guillaumon@gmail.com>
2023-10-01 19:28:55 -0300

    added main.py, No.py, Mole.py files

---
+++ src/python/No.py
@@ -0,0 +1,8 @@
+class No:
+    def __int__(self, x, y, conhecido=False, dx=None, dy=None):
+        self.x = x
+        self.y = y
+        self.pos = (x, y)
+        self.conhecido = conhecido
+        self.dx =dx
+        self.dy =dy
